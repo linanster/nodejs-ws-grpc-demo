654ae69c3540c50922671c9ad8d66e9db1dd57b0
Aily Le <aily.le@on-running.com>
2025-02-18 09:55:30 +0800

    Update

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,6 +1,6 @@
 import { Server } from "./server";
 import { createGrpcClient } from "../utils/grpcUtils";
 
-const protoPackage = createGrpcClient("./src/proto/auth.proto");
 const server = new Server("auth");
+const protoPackage = createGrpcClient("./src/proto/auth.proto");
 server.startGrpc(50051, protoPackage.auth.AuthService.service);
--- src/server/gateway.ts
+++ src/server/gateway.ts
@@ -1,4 +1,7 @@
+import { createGrpcClient } from "../utils/grpcUtils";
 import { Server } from "./server";
 
 const server = new Server("gateway");
 server.startWs(8080); // 传递端口号给 start 方法
+const protoPackage = createGrpcClient("./src/proto/gateway.proto");
+server.startGrpc(50050, protoPackage.gateway.GatewayService.service);
